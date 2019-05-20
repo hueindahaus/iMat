001f69ac7b6ada32856938d778bb2028103b7573
MiztaOak <ek.johan98@gmail.com>
2019-05-20 12:10:42 +0200

    Förbättrat designen av historie elementen
    Har gjort så att historie elementen visar upp korten korrekt och så att de går att fälla ut dem ifall man inte vill scrolla.

--- projekt/src/sample/HistoryItem.java
+++ projekt/src/sample/HistoryItem.java
@@ -1,14 +1,18 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
-import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Order;
 
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.Map;
 
@@ -19,6 +23,13 @@ public class HistoryItem extends AnchorPane {
     private Label orderDate;
     @FXML
     private FlowPane orderMain;
+
+    @FXML
+    private ImageView expandButton;
+    @FXML
+    private ScrollPane historyScroll;
+
+    private Order order;
 
     public HistoryItem(Map<String, ProductListItem> productListItemMap, Order order, ProductSearchController parent){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("history_item.fxml"));
@@ -35,5 +46,20 @@ public class HistoryItem extends AnchorPane {
         orderNumber.setText("Ordernummer: " + Integer.toString(order.getOrderNumber()));
         //order.getItems().forEach(e -> orderMain.getChildren().add(productListItemMap.get(e.getProduct().getName())));
         order.getItems().forEach(e -> orderMain.getChildren().add(new ProductListItem(e,parent)));
+
+
+        this.order = order;
+    }
+
+    @FXML
+    public void changeHistorySize(){
+        if(historyScroll.getPrefHeight() != 350){
+            historyScroll.setPrefHeight(350);
+            expandButton.setImage(new Image(getClass().getResource("../icons/baseline_expand_more_black_18dp.png").toExternalForm()));
+        } else{
+            int height = (order.getItems().size()%3)*300 + 20;
+            historyScroll.setPrefHeight(height);
+            expandButton.setImage(new Image(getClass().getResource("../icons/baseline_expand_less_black_18dp.png").toExternalForm()));
+        }
     }
 }
