9b76c8d135f74f4413960cc91f19495898f46871
MiztaOak <ek.johan98@gmail.com>
2019-05-22 12:55:31 +0200

    Added a sorting function to the main page to get the most bought items

--- projekt/src/sample/MainPage.java
+++ projekt/src/sample/MainPage.java
@@ -4,9 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Order;
+import se.chalmers.cse.dat216.project.Product;
 import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,9 +30,7 @@ public class MainPage extends AnchorPane {
             throw new RuntimeException(exception);
         }
 
-        weeksOffers.getChildren().add(productListItemMap.get("Kaffe"));
-        weeksOffers.getChildren().add(productListItemMap.get("Västerbotten"));
-        weeksOffers.getChildren().add(productListItemMap.get("Grapefruit"));
+
         IMatDataHandler dataHandler = IMatDataHandler.getInstance();
         if(dataHandler.getOrders().size() > 0){
             List<ShoppingItem> items = dataHandler.getOrders().get(dataHandler.getOrders().size()-1).getItems();
@@ -40,5 +42,32 @@ public class MainPage extends AnchorPane {
                 }
             }
         }
+    }
+
+    private List<Product> getMostBought (List<Order> orders){
+        Map<Product, Integer> productIntegerMap = new HashMap<>();
+        List<Product> products = new ArrayList<>();
+        for(Order order: orders){
+            for(ShoppingItem item: order.getItems()){
+                if(!productIntegerMap.containsKey(item.getProduct())){
+                    productIntegerMap.put(item.getProduct(),1);
+                }else{
+                    productIntegerMap.put(item.getProduct(),1);
+                    products.add(item.getProduct());
+                }
+            }
+        }
+
+        for(int i = 0; i < products.size()-1; i++){
+            for(int j = i +1; j < products.size(); j++ ){
+                if(productIntegerMap.get(products.get(i)) < productIntegerMap.get(products.get(j))){
+                    Product tmp = products.get(i);
+                    products.set(i,products.get(j));
+                    products.set(j,tmp);
+                }
+            }
+        }
+
+        return products.subList(0,2);
     }
 }
